87f3ae70c46360fbf2bd53763fe2c830c17abaeb
bgnatowski <bgnatowski00@gmail.com>
2023-07-14 17:00:39 +0200

    change get in stock from boolean to List of InventoryResponseDto

--- inventory-service/src/main/java/pl/bgnat/inventory/domain/InventoryController.java
+++ inventory-service/src/main/java/pl/bgnat/inventory/domain/InventoryController.java
@@ -2,7 +2,11 @@ package pl.bgnat.inventory.domain;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.bgnat.inventory.dto.InventoryResponseDto;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/inventory")
@@ -10,9 +14,11 @@ import org.springframework.web.bind.annotation.*;
 class InventoryController {
 	private final InventoryService inventoryService;
 
-	@GetMapping("/{skuCode}")
+
+	//http://localhost:8086/api/v1/inventory/iphone-13,iphone13-red
+	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
-	boolean isInStock(@PathVariable("skuCode") String skuCode){
-		return inventoryService.isInStock(skuCode);
+	ResponseEntity<List<InventoryResponseDto>> isInStock(@RequestParam("skuCode") List<String> skuCodes){
+		return ResponseEntity.ok(inventoryService.isInStock(skuCodes));
 	}
 }
--- inventory-service/src/main/java/pl/bgnat/inventory/domain/InventoryService.java
+++ inventory-service/src/main/java/pl/bgnat/inventory/domain/InventoryService.java
@@ -3,14 +3,22 @@ package pl.bgnat.inventory.domain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.bgnat.inventory.dto.InventoryResponseDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
 	private final InventoryRepository inventoryRepository;
+	private final InventoryDtoMapper inventoryDtoMapper;
 
 	@Transactional(readOnly = true)
-	public boolean isInStock(String skuCode) {
-		return inventoryRepository.findBySkuCode(skuCode).isPresent();
+	public List<InventoryResponseDto> isInStock(List<String> skuCode) {
+		return inventoryRepository.findBySkuCodeIn(skuCode)
+				.stream()
+				.map(inventoryDtoMapper)
+				.collect(Collectors.toList());
 	}
 }
