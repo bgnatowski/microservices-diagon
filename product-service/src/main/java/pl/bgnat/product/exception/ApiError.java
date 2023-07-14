4ada2b2f37dc2125a821e7a96cbc86fc0af5cf5d
bgnatowski <bgnatowski00@gmail.com>
2023-07-14 14:48:12 +0200

    fixes in product service

---
+++ product-service/src/main/java/pl/bgnat/product/exception/ApiError.java
@@ -0,0 +1,11 @@
+package pl.bgnat.product.exception;
+
+import java.time.LocalDateTime;
+
+public record ApiError(
+		String path,
+		String message,
+		int statusCode,
+		LocalDateTime localDateTime
+) {
+}
