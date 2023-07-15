a4aa3f4945f0125fcc85a7231143c5924186bfa4
bgnatowski <bgnatowski00@gmail.com>
2023-07-15 17:31:45 +0200

    inventory-service: changed commandlinerunner from main class to own DataLoader class

--- inventory-service/src/main/java/pl/bgnat/inventory/InventoryApplication.java
+++ inventory-service/src/main/java/pl/bgnat/inventory/InventoryApplication.java
@@ -1,11 +1,7 @@
 package pl.bgnat.inventory;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import pl.bgnat.inventory.domain.Inventory;
-import pl.bgnat.inventory.domain.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryApplication {
@@ -13,19 +9,4 @@ public class InventoryApplication {
 		SpringApplication.run(InventoryApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner loadData(InventoryRepository inventoryRepository){
-		return args -> {
-			Inventory inventory = new Inventory();
-			inventory.setSkuCode("iphone_13");
-			inventory.setQuantity(100);
-
-			Inventory inventory1 = new Inventory();
-			inventory1.setSkuCode("iphone_13_red");
-			inventory1.setQuantity(0);
-
-			inventoryRepository.save(inventory);
-			inventoryRepository.save(inventory1);
-		};
-	}
 }
---
+++ inventory-service/src/main/java/pl/bgnat/inventory/utils/DataLoader.java
@@ -0,0 +1,26 @@
+package pl.bgnat.inventory.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import pl.bgnat.inventory.domain.Inventory;
+import pl.bgnat.inventory.domain.InventoryRepository;
+
+@Component
+@RequiredArgsConstructor
+public class DataLoader implements CommandLineRunner {
+	private final InventoryRepository inventoryRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Inventory inventory = new Inventory();
+		inventory.setSkuCode("iphone_13");
+		inventory.setQuantity(100);
+
+		Inventory inventory1 = new Inventory();
+		inventory1.setSkuCode("iphone_13_red");
+		inventory1.setQuantity(0);
+
+		inventoryRepository.save(inventory);
+		inventoryRepository.save(inventory1);
+	}
+}
