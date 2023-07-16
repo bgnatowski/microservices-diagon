68fc5afc510038c20b4b6d08978e0b88897e5127
bgnatowski <bgnatowski00@gmail.com>
2023-07-16 16:22:29 +0200

    api-gateway: add SecurityConfig for OAuth2 with issuer-uri keycloack

---
+++ api-gateway/src/main/java/pl/bgnat/api/gateway/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package pl.bgnat.api.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuerUri;
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+		serverHttpSecurity
+				.csrf(csrfSpec -> csrfSpec.disable())
+				.authorizeExchange(exchange ->
+						exchange.pathMatchers("/eureka/**")
+								.permitAll()
+								.anyExchange()
+								.authenticated()
+				)
+				.oauth2ResourceServer(oauth2 -> oauth2
+						.jwt(withDefaults()));
+		return serverHttpSecurity.build();
+	}
+
+	@Bean
+	public ReactiveJwtDecoder jwtDecoder() {
+		return ReactiveJwtDecoders.fromIssuerLocation(issuerUri);
+	}
+}
