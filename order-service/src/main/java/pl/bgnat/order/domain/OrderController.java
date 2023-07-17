40f5aedecc268c682b12e9b92367f05bee6b167b
bgnatowski <bgnatowski00@gmail.com>
2023-07-17 16:58:22 +0200

    order-service: add retry

--- order-service/src/main/java/pl/bgnat/order/domain/OrderController.java
+++ order-service/src/main/java/pl/bgnat/order/domain/OrderController.java
@@ -1,38 +1,41 @@
 package pl.bgnat.order.domain;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.actuate.endpoint.web.annotation.WebEndpoint;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.bgnat.order.dto.OrderRequest;
+import pl.bgnat.order.exception.order.OutOfStockException;
 
 import java.util.concurrent.CompletableFuture;
 
-import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
 
 @RestController
 @RequestMapping("api/v1/order")
 @RequiredArgsConstructor
-class OrderController {
+public class OrderController {
 	private final OrderService orderService;
 
 	@PostMapping("/add")
 	@ResponseStatus(CREATED)
 	@CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod") //name is the same key as in properties
+	@Retry(name = "inventory")
 	@TimeLimiter(name = "inventory")
 	public CompletableFuture<ResponseEntity<String>> placeOrder(@RequestBody OrderRequest orderRequest) {
 		return CompletableFuture.supplyAsync(() -> {
-					String orderStatus = orderService.placeOrder(orderRequest);
-					return ResponseEntity
-							.status(CREATED)
-							.body(orderStatus);
-				});
+			String orderStatus = orderService.placeOrder(orderRequest);
+			return ResponseEntity
+					.status(CREATED)
+					.body(orderStatus);
+		});
 	}
 
-	public CompletableFuture<ResponseEntity<String>> fallbackMethod(OrderRequest orderRequest, Throwable throwable) {
+	public CompletableFuture<ResponseEntity<String>> fallbackMethod(OrderRequest orderRequest, OutOfStockException exception) {
 		return CompletableFuture.supplyAsync(() ->
 				ResponseEntity
 						.status(SERVICE_UNAVAILABLE)
