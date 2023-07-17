4e3ae7fc63c60adf4cff12299e2f9d7f17174c18
bgnatowski <bgnatowski00@gmail.com>
2023-07-17 12:44:05 +0200

    inventory-service: add slf4

--- inventory-service/src/main/java/pl/bgnat/inventory/domain/InventoryService.java
+++ inventory-service/src/main/java/pl/bgnat/inventory/domain/InventoryService.java
@@ -1,6 +1,8 @@
 package pl.bgnat.inventory.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.bgnat.inventory.dto.InventoryDto;
@@ -10,12 +12,17 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 	private final InventoryRepository inventoryRepository;
 	private final InventoryDtoMapper inventoryDtoMapper;
 
 	@Transactional(readOnly = true)
+	@SneakyThrows
 	public List<InventoryDto> isInStock(List<String> skuCode) {
+//		log.info("Wait Started"); //tests
+//		Thread.sleep(40000);
+//		log.info("Wait Ended");
 		return inventoryRepository.findBySkuCodeIn(skuCode)
 				.stream()
 				.map(inventoryDtoMapper)
