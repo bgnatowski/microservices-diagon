ffbe6d1661c0993c491b94408905946b4c1013e8
bgnatowski <bgnatowski00@gmail.com>
2023-07-19 11:05:41 +0200

    change services that use postgres to use one database

--- init_db.sql
+++ init_db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE customer;
-CREATE DATABASE fraud;
-CREATE DATABASE inventory;
-CREATE DATABASE orders;
+-- CREATE DATABASE customer;
+-- CREATE DATABASE fraud;
+-- CREATE DATABASE inventory;
+-- CREATE DATABASE orders;
