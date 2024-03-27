18ff0b371102440a2d5ee782bdef09646c9d9cb1
bgnatowski_adm <b.gnatowski@sii.pl>
2024-03-27 13:00:01 +0100

    working good local and docker

--- api-gateway/src/main/java/pl/bgnat/api/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/pl/bgnat/api/gateway/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package pl.bgnat.api.gateway.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
